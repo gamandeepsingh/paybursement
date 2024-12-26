b8a6c228690d2694e47165e6b99b9af436cb00cf
gamandeepsingh <gamandeepsingh4@gmail.com>
2024-12-26 20:43:42 +0530

    Features: add logout functionality and improve error handling in user authentication

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -5,58 +5,91 @@ import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { signOutUserSuccess } from "@/redux/user/userSlice";
+import { useLocation, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
   const currentUser = useSelector((state: RootState) => state.user.currentUser);
-  
   const [openBox, setOpenBox] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [loading,setLoading] = useState(false);
 
-  const handleSignOut = ()=>{
-    dispatch(signOutUserSuccess()); // Clear Redux state
-    localStorage.removeItem("token"); // Clear token
-    localStorage.removeItem("user"); // Clear user data
-    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; // Clear cookie
-    setTimeout(() => {
-      window.location.href = "/sign-in"; // Redirect after cleanup
-    }, 200);
-  }
+  useEffect(() => {
+    if (location.state?.showToast) {
+      toast.success("Successfully Logged in");
+    }
+  }, [location.state]);
+
+  const handleSignOut = async () => {
+    setLoading(true);
+    try {
+      await axios.post("/api/user/logout");
+      dispatch(signOutUserSuccess());
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      navigate("/sign-in");
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }finally{
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="w-screen min-h-[600px] mt-8 bg-primarylight dark:bg-primaryDark relative">
       <div className="bg-secondary dark:bg-primary">
         <div className="flex justify-between items-center py-5 px-10">
-          <h1 className="text-2xl font-bold">Dashboard / <span className="text-xs">PB</span></h1>
+          <h1 className="text-2xl font-bold">
+            Dashboard / <span className="text-xs">PB</span>
+          </h1>
           <div className="flex items-center gap-5">
-            <h1 className="text-sm hidden sm:block">Welcome, <span className="text-base font-black">{currentUser?.fullname?.firstname}</span></h1>
+            <h1 className="text-sm hidden sm:block">
+              Welcome,{" "}
+              <span className="text-base font-black">
+                {currentUser?.fullname?.firstname}
+              </span>
+            </h1>
             <img
-              src={currentUser?.profilePicture || `https://ui-avatars.com/api/?name=${currentUser?.fullname?.firstname}+${currentUser?.fullname?.lastname}`}
+              src={
+                currentUser?.profilePicture ||
+                `https://ui-avatars.com/api/?name=${currentUser?.fullname?.firstname}+${currentUser?.fullname?.lastname}`
+              }
               alt=""
               className="w-10 h-10 rounded-full cursor-pointer"
               onClick={() => setOpenBox((pv) => !pv)}
             />
           </div>
-            {openBox && (
-              <motion.div
-                initial={{ opacity: 0, scale: 0.9 }}
-                animate={{ opacity: 1, scale: 1 }}
-                exit={{ opacity: 0, scale: 0.9 }}
-                transition={{ duration: 0.2 }}
-                className="z-50 absolute right-10 top-16 bg-white dark:bg-secondary shadow-lg rounded-lg p-5 w-52"
-              >
-                <h1 className="text-lg font-bold">Profile</h1>
-                <hr className="my-3 border-neutral-300" />
-                <p className="text-sm">
-                  <span className="font-bold">Welcome,</span> {currentUser?.fullname?.firstname} {currentUser?.fullname?.lastname}
-                </p>
-                <button
+          {openBox && (
+            <motion.div
+              initial={{ opacity: 0, scale: 0.9 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.9 }}
+              transition={{ duration: 0.2 }}
+              className="z-50 absolute right-10 top-16 bg-white dark:bg-secondary shadow-lg rounded-lg p-5 w-52"
+            >
+              <h1 className="text-lg font-bold">Profile</h1>
+              <hr className="my-3 border-neutral-300" />
+              <p className="text-sm">
+                <span className="font-bold">Welcome,</span>{" "}
+                {currentUser?.fullname?.firstname}{" "}
+                {currentUser?.fullname?.lastname}
+              </p>
+              <button
                 className="w-full bg-primary text-white py-2 rounded-lg mt-3"
                 onClick={handleSignOut}
-                >
-                  Sign Out
-                </button>
-              </motion.div>
-            )}
-          </div>
+                disabled={loading}
+              >
+                {
+                  loading ? "Signing out..." : "Sign Out"
+                }
+              </button>
+            </motion.div>
+          )}
+        </div>
       </div>
       <div className="container grid grid-cols-1 md:grid-cols-2 gap-4 place-content-center pt-20 sm:pt-10 max-w-7xl mx-auto">
         <div className="text-left flex flex-col justify-center gap-5 mx-10">
@@ -80,13 +113,11 @@ const Dashboard = () => {
 
 export default Dashboard;
 
-const BlockInTextCard = ({ examples }:{
-  examples: string[]
-}) => {
+const BlockInTextCard = ({ examples }: { examples: string[] }) => {
   return (
     <div className="w-full max-w-xl space-y-6">
-        <Typewrite examples={examples} />
-        <hr className="border-neutral-300" />
+      <Typewrite examples={examples} />
+      <hr className="border-neutral-300" />
     </div>
   );
 };
@@ -99,9 +130,7 @@ const MAIN_FADE_DURATION = 0.25;
 
 const SWAP_DELAY_IN_MS = 5500;
 
-const Typewrite = ({ examples }:{
-  examples: string[]
-}) => {
+const Typewrite = ({ examples }: { examples: string[] }) => {
   const [exampleIndex, setExampleIndex] = useState(0);
 
   useEffect(() => {
